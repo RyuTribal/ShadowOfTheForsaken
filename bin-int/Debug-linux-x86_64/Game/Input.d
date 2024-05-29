fae9523b742316f933a666585e061b7819a29b76
Ivan Sedelkin <ivan.sedelkin@rocksigma.com>
2024-05-29 18:15:23 +0200

    Added gitignore

--- bin-int/Debug-linux-x86_64/Game/Input.d
+++
@@ -1,3 +0,0 @@
-bin-int/Debug-linux-x86_64/Game/Input.o: src/Core/Input.cpp \
- /usr/include/stdc-predef.h
-/usr/include/stdc-predef.h:
--- bin-int/Debug-linux-x86_64/Game/Log.d
+++
@@ -1,3 +0,0 @@
-bin-int/Debug-linux-x86_64/Game/Log.o: src/Core/Log.cpp \
- /usr/include/stdc-predef.h
-/usr/include/stdc-predef.h:
