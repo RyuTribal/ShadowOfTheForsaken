3a0595321faa786088bd585fe0c47d7f1382d809
Irregular Bismuth <elliot.s@live.se>
2024-09-10 21:28:30 +0200

    added clang-format update

--- src/ImGui/ImGuiLayer.h
+++ src/ImGui/ImGuiLayer.h
@@ -8,16 +8,16 @@ namespace SOF {
 
 class ImGuiLayer
 {
-public:
-  static void Init();
-  static void Shutdown();
+  public:
+    static void Init();
+    static void Shutdown();
 
-  static void OnEvent(Event &e);
+    static void OnEvent(Event &e);
 
-  static void Begin();
-  static void End();
+    static void Begin();
+    static void End();
 
-  static void BlockEvents(bool block);
+    static void BlockEvents(bool block);
 };
 
 }// namespace SOF
--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,19 @@
 #include "Core/Game.h"
 #include "Core/Window.h"
 
-int main(){
-   
+int main()
+{
+
     SOF::Log::Init();
     SOF_WARN("Entrypoint", "Log initialized!");
-    
+
     SOF::WindowProps window_settings{};
     window_settings.Title = "Shadow of the Forsaken";
     window_settings.VSync = true;
     window_settings.Height = 720;
     window_settings.Width = 1280;
 
-    SOF::Game* game_instance = SOF::Game::CreateGame(window_settings);
+    SOF::Game *game_instance = SOF::Game::CreateGame(window_settings);
 
     game_instance->Start();
 
